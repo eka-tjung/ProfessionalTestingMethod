40b3603fe10d8af46397dc86ad41428db5481ad7
Eka Tjung <eka.tjung@gmail.com>
2017-10-22 17:33:21 -0700

    Adding Day5Example

---
+++ Day5Example/SampleUsage/Program.cs
@@ -0,0 +1,23 @@
+﻿using Day5Example;
+using System;
+namespace SampleUsage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please provide a radius for the circle");
+            uint radius = Convert.ToUInt16(Console.ReadLine());
+            Console.WriteLine();
+
+            Circle newCircle = new Circle(radius);
+            Console.WriteLine("Radius: {0}", newCircle.Radius);
+            Console.WriteLine("Area: {0}", newCircle.Area);
+            Console.WriteLine("Circumference: {0}", newCircle.Circumference);
+
+            // Keep console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}

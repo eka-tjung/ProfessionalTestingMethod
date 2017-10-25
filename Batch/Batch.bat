fe08b461b4b38751e49d42145df0c49e71e819bf
Eka Tjung <eka.tjung@gmail.com>
2017-10-24 19:16:11 -0700

    Add Batch scripts

---
+++ Batch/Batch.bat
@@ -0,0 +1,11 @@
+echo %~dp0
+SET batchDir=%~dp0
+echo %batchDir%
+MD %batchDir%NewDir
+IF EXIST %batchDir%NewDir (
+RD /s /q %batchDir%NewDir
+)
+MD %batchDir%NewDir
+Copy %batchDir%ToCopy.txt %batchDir%NewDir\Copied.txt
+dir > %batchDir%NewDir\NewFile.txt
+REM RD /s /q %batchDir%NewDir
